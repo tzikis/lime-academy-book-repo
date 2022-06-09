118170764bda7939742ba9a373061c0c918516fd
Vasilis Georgitzikis <tzikis@gmail.com>
2022-06-09 14:30:56 +0300

    Finished ether.js sample scripts

--- scripts/interact-rinkeby.js
+++ scripts/interact-rinkeby.js
@@ -1,20 +1,25 @@
 const hre = require("hardhat");
 const USElection = require('../artifacts/contracts/USElection.sol/USElection')
 
+const dotenv = require("dotenv");
+
+dotenv.config({path: __dirname + '/../.env'});
+const { INFURA_API_KEY, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 const run = async function() {
 
-	const provider = new hre.ethers.providers.JsonRpcProvider("http://localhost:8545")
+	const provider = new hre.ethers.providers.InfuraProvider("rinkeby", INFURA_API_KEY)
 	const latestBlock = await provider.getBlock("latest")
 	console.log(latestBlock.hash)
 
-    const wallet = new hre.ethers.Wallet("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", provider);
+    const wallet = new hre.ethers.Wallet(PRIVATE_KEY, provider);
 	const balance = await wallet.getBalance();
 
     console.log(hre.ethers.utils.formatEther(balance, 18))
     console.log(balance.toString())
 
-	const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+	
+	const contractAddress = "0x5155bE53a3144BAf6D2D8a3123Ac1914d5FDF76F"
 	const electionContract = new hre.ethers.Contract(contractAddress, USElection.abi, wallet)
 
     const USElectionFactory = await hre.ethers.getContractFactory("USElection");
@@ -41,6 +46,7 @@ const run = async function() {
 
 	const currentLeader = await electionContract.currentLeader()
 	console.log("Current leader", currentLeader)
+	
 }
 
 run()
